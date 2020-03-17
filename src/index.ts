9735a70b51dd5a14a9dbb652a4abecbbdd037a8f
taoqf <tao_qf@163.com>
2020-03-17 10:29:25 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import { read, utils } from 'xlsx';
 
-export default function an24(param1: string) {
+/**
+ * excel转json
+ */
+export default function excel2json<T>(data: Buffer) {
+	const wb = read(data, {
+		type: 'buffer'
+	});
+	return wb.SheetNames.map((sheet_name) => {
+		return utils.sheet_to_json<T>(wb.Sheets[sheet_name], {
+			raw: true
+		});
+	});
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,20 @@
 const test = require('ava');
+const { join } = require('path');
+const { promises } = require('fs');
 
+const { readFile } = promises;
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('convert xlsx to json', async (t) => {
+	const data = await readFile(join(__dirname, 'test.xlsx'));
+	const r = a(data);
+	t.deepEqual(r, [[{
+		Name: 'alice',
+		Sex: 0,
+		Age: 18
+	}, {
+		Name: 'john',
+		Sex: 1,
+		Age: 22
+	}]]);
 });
